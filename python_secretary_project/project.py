ef43777722bc06259025736320425567ef3ab37e
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-12 00:31:19 +0900

    ## 루비 데이터 스크래핑 update (날씨, 회화, 국내& 해외뉴스)

--- python_secretary_project/project.py
+++ python_secretary_project/project.py
@@ -1,9 +1,7 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
-import time
 import re
+import datetime
 
 headers = {
     "User-Agent":
@@ -16,6 +14,14 @@ def create_soup(url):
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
     return soup
+
+
+############### 조회 날짜 ###############
+def realtime_print():
+    now = datetime.datetime.now()
+    nowDate = now.strftime('%Y년 %m월 %d일 (%a)')
+    print(f"안녕하세요. 오늘은 {nowDate} 입니다.")  # 2021-05-112
+    print()
 
 
 ############### 날씨 ###############
@@ -54,34 +60,8 @@ def scrap_weather():
     print(
         f"오전 강수확률 {rainy_data[0].get_text()}% / 오후 강수확률 {rainy_data[1].get_text()}%"
     )
-    print()
     print(f"미세먼지 {dust_data[0].get_text()}")
     print(f"초미세먼지 {dust_data[1].get_text()}")
-    print()
-
-
-############### News ###############
-def scrap_news(sector):
-    news_url = "https://news.naver.com/"
-    soup = create_soup(news_url)
-
-    # 뉴스 데이터
-    news_data = soup.find("div", attrs={"id": f"section_{sector}"})
-    # 섹터 이름
-    sector_name = news_data.find("h4", attrs={"class": "tit_sec"}).a.get_text()
-    # 헤드라인 목록
-    news_list = news_data.find("ul", attrs={
-        "class": "mlist2 no_bg"
-    }).find_all("li")
-
-    # 출력
-    print(f"[국내 {sector_name} 헤드라인 뉴스]")
-    for idx, article in enumerate(news_list):
-        headline = article.a.get_text().strip()
-        news_link = article.a["href"]
-        print(f"{idx+1}. {headline}")
-        print(f"   (링크 : {news_link} )")
-
     print()
 
 
@@ -106,20 +86,80 @@ def scrap_english():
     print()
 
 
+############### 국내 News ###############
+# sector
+# politics : 정치
+# economy : 경제
+# it : IT/과학
+# society : 사회
+# life : 생활/문화
+# world : 세계
+def scrap_news(sector):
+    news_url = "https://news.naver.com/"
+    soup = create_soup(news_url)
+
+    # 뉴스 데이터
+    news_data = soup.find("div", attrs={"id": f"section_{sector}"})
+    # 섹터 이름
+    sector_name = news_data.find("h4", attrs={"class": "tit_sec"}).a.get_text()
+    # 헤드라인 목록
+    news_list = news_data.find("ul", attrs={
+        "class": "mlist2 no_bg"
+    }).find_all("li")
+
+    # 출력
+    print(f"[국내 {sector_name} 헤드라인 뉴스]")
+    for idx, article in enumerate(news_list):
+        headline = article.a.get_text().strip()
+        news_link = article.a["href"]
+        print(f"{idx+1}. {headline}")
+        print(f"   (링크 : {news_link} )")
+
+    print()
+
+
+############### 해외 News ###############
+# CNBC Trendig News
+def scrap_global_news(sector):
+    news_url = f"https://www.cnbc.com/{sector}/"
+    soup = create_soup(news_url)
+
+    # 섹터 이름
+    sector_name = soup.find("h1", attrs={
+        "class": "PageHeader-title"
+    }).get_text()
+    # 트랜딩 뉴스 데이터
+    news_data = soup.find("ul", attrs={"class": "TrendingNow-storyContainer"})
+    # 헤드라인 목록
+    news_list = news_data.find_all("li")
+
+    # 출력
+    print(f"[해외 실시간 CNBC 헤드라인 뉴스]")
+    for idx, article in enumerate(news_list):
+        headline = article.a.get_text().strip()
+        news_link = article.a["href"]
+        print(f"{idx+1}. {headline}")
+        print(f"   (링크 : {news_link} )")
+
+    print()
+
+
+# TODO:  경제 관련 데이터 - 원달러 환율, 국내/ 미국 대표 지수 변동률
+
 if __name__ == "__main__":
+    # 현재 시간 출력
+    realtime_print()
+
     # 오늘의 날씨 정보 가져오기
     scrap_weather()
 
-    #### 분야별 뉴스 가져오기
-    # politics : 정치
-    # economy : 경제
-    # it : IT/과학
-    # society : 사회
-    # life : 생활/문화
-    # world : 세계
+    # 영어 회화
+    scrap_english()
+
+    #### 국내 분야별 뉴스 가져오기
     scrap_news("politics")
     scrap_news("economy")
     scrap_news("it")
 
-    # 영어 회화
-    scrap_english()
+    #### 해외 현재 트렌딩 뉴스
+    scrap_global_news("economy")
